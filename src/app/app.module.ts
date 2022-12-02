c8088eab9255b260a39091e6f80b911ef2174745
vandelio <stefanvandel@gmail.com>
2022-12-02 14:50:50 +0100

    4 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CustomGaugeComponent } from './custom-gauge/custom-gauge.component';
 import { SvgGaugeComponent } from './svg-gauge/svg-gauge.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    CustomGaugeComponent,
-    SvgGaugeComponent,
-  ],
+  declarations: [AppComponent, HelloComponent, SvgGaugeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
