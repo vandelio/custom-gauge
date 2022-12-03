ab96e56eaf048c12a9d8b44d1dcbbdb1960bd2df
vandelio <stefanvandel@gmail.com>
2022-12-03 22:20:16 +0100

    3 files modified

--- src/app/svg-gauge/svg-gauge.component.ts
+++ src/app/svg-gauge/svg-gauge.component.ts
@@ -7,32 +7,73 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SvgGaugeComponent implements OnInit {
   @Input() value: number = 0;
+  gaugeSettings = [
+    {
+      color: '#3BCE58',
+      needleRotatingValue: -110,
+      displayValue: '1',
+      titleSuffix: 'Good',
+    },
+    {
+      color: '#E78002',
+      needleRotatingValue: -60,
+      displayValue: '2',
+      titleSuffix: 'Medium',
+    },
+    {
+      color: '#E78002',
+      needleRotatingValue: 0,
+      displayValue: '3',
+      titleSuffix: 'Medium',
+    },
+    {
+      color: '#E78002',
+      needleRotatingValue: 60,
+      displayValue: '4',
+      titleSuffix: 'Medium',
+    },
+    {
+      color: '#DB0300',
+      needleRotatingValue: 110,
+      displayValue: '5',
+      titleSuffix: 'Poor',
+    },
+  ];
+
+  displayValueOfTotal = '5';
   colorArray = ['#3BCE58', '#E78002', '#DB0300'];
-  needleValue: number = -120;
-  needlePosition: string = 'rotate(-110 80 80)';
+  needleRotatingValue: number = -110; // starting point
+  needlePosition: string = 'rotate(' + this.needleRotatingValue + ' 80 80)'; // starting point
   transformOrigin: number = 80;
+  title: string = "your asset's health is ";
+  setting;
 
   constructor() {}
 
   ngOnInit() {
-    let interv = setInterval(() => {
-      this.needleValue += 2;
-      const pos = this.transformOrigin;
-      this.needlePosition = `rotate(${this.needleValue} ${pos} ${pos})`;
-      if (this.needleValue === this.needleTarget(this.value)) {
-        clearInterval(interv);
-      }
-    }, 10);
+    this.setting = this.getGaugeSettings();
+
+    // Move needle until final position
+    /*let movingGaugeNeedle = setInterval(() => {
+      this.needleRotatingValue += 2;
+      this.setNeedlePosition();
+      if (this.needleRotatingValue === this.setting.needleRotatingValue)
+        clearInterval(movingGaugeNeedle);
+    }, 10);*/
   }
-  needleTarget(value) {
-    if (value === 0.1) return -110;
-    if (value === 0.2) return -90;
-    if (value === 0.3) return -60;
-    if (value === 0.4) return -30;
-    if (value === 0.5) return 0;
-    if (value === 0.6) return 30;
-    if (value === 0.7) return 60;
-    if (value === 0.8) return 90;
-    if (value === 0.9) return 110;
+  setNeedlePosition() {
+    const pos = this.transformOrigin;
+    this.needlePosition = `rotate(${this.needleRotatingValue} ${pos} ${pos})`;
+  }
+  getGaugeSettings() {
+    console.log('index', this.getIndexByInputValue());
+    return this.gaugeSettings[this.getIndexByInputValue()];
+  }
+  getIndexByInputValue() {
+    if (this.value <= 0.2) return 0;
+    if (this.value > 0.2 && this.value <= 0.4) return 1;
+    if (this.value > 0.4 && this.value < 0.6) return 2;
+    if (this.value >= 0.6 && this.value < 0.8) return 3;
+    if (this.value >= 0.8) return 4;
   }
 }
